6b3514644317005d87d93b91bd9ed741b98511bc
Francisco Prada <fmamp24@gmail.com>
2025-06-01 16:53:37 +0100

    updated no rejs

--- main.sh
+++ main.sh
@@ -12,31 +12,21 @@ select opt in "${options[@]}"; do
             docker build -t dilithium_image ./Dilithium
 
             echo "Running Dilithium container in detached mode..."
-            # Create a local 'results' folder if it doesn't exist
             mkdir -p Dilithium/results_no_rej/mode2
             mkdir -p Dilithium/results_no_rej/mode3
             mkdir -p Dilithium/results_no_rej/mode5
 
-            # Run the container in detached mode with a fixed name.
-            docker run --cpuset-cpus="0" --cpus="1" --name dilithium_container -d dilithium_image
+            docker run --cpuset-cpus="0" --name dilithium_container -d dilithium_image
 
             echo "Waiting for container to finish..."
             docker wait dilithium_container
 
             echo "Copying results from container..."
 
-            # Copy the results files from the container to your host's results folder.
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode2.csv Dilithium/results_no_rej/mode2/dilithium_times.csv
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode2.png Dilithium/results_no_rej/mode2/dilithium_times.png
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode3.csv Dilithium/results_no_rej/mode3/dilithium_times.csv
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode3.png Dilithium/results_no_rej/mode3/dilithium_times.png
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode5.csv Dilithium/results_no_rej/mode5/dilithium_times.csv
-            docker cp dilithium_container:/results_no_rej/dilithium_times_mode5.png Dilithium/results_no_rej/mode5/dilithium_times.png
-
-
-
-            # Clean up the container
-            # docker rm dilithium_container
+            for mode in 2 3 5; do
+                docker cp dilithium_container:/results_no_rej/dilithium_times_mode${mode}.csv Dilithium/results_no_rej/mode${mode}/dilithium_times.csv
+                docker cp dilithium_container:/results_no_rej/dilithium_times_mode${mode}.png Dilithium/results_no_rej/mode${mode}/dilithium_times.png
+            done
 
             echo "Results have been downloaded to the local 'results' folder."
             break
